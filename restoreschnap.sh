48f3686d4a05e6925ad8e19df78d09d8bc9454e7
hhoefling <penny@gmx.eu>
2023-02-25 22:22:39 +0100

    Update restoreschnap.sh

--- restoreschnap.sh
+++ restoreschnap.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 SELF=$(basename $0)
-base=/var/www/html
 
 dn=${1:-"None"}
-fromdir="${base}/$dn"
-todir="${base}/openWB"
 debug=${2:-0}
 
 
@@ -28,106 +25,153 @@ function meld()
   echo "$1"
 }
 
-deb "$SELF starting ($debug)"
+
+base=/var/www/html
+fromdir="${base}/$dn"
+todir="${base}/openWB"
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 
-if [ ! -d $fromdir ] ; then
-  Error "Verzeichnis $fromdir existiert nicht."
-fi
+function checkIoRedirection()
+{
+ dir=${1:-$fromdir}
 
-meld "Schnappschuss $fromdir wiederherstellen." 
-meld "Quelle: $fromdir"
-meld "Ziel  : $todir"
-sleep 1 
+ meld "Check for pipe-Error in Scripten in $dir."
 
-cd $base
-
-
-meld "Check for pipe-Error in Scripten."
-deb "Suche in $fromdir/runs/ "
-if grep -q -r -e  "sudo.*python3.*OPENWBBASEDIR/runs/rfid/readrfid.py.*-d event0\s*&" $fromdir/runs/* ; then
+ if grep -q -r -e  "sudo.*python3.*OPENWBBASEDIR/runs/rfid/readrfid.py.*-d event0\s*&" $dir/runs/* ; then
   echo "********************** need patch *********************"
-  grep -ri -n -e  "sudo python3.*$OPENWBBASEDIR/runs/rfid/readrfid.py.*-d event0\s*&" $fromdir/runs/* 
+  grep -ri -n -e  "sudo python3.*$OPENWBBASEDIR/runs/rfid/readrfid.py.*-d event0\s*&" $dir/runs/* 
   echo " add \" &lt;/dev/null >/dev/null 2>&1 \" before the &"
   Error "Abbruch. Bitte erst korrigieren. atreboot.sh bleibt sonst hängen"
-fi 
-if grep -q -r -e  "sudo python3.*OPENWBBASEDIR/runs/rfid/readrfid.py.*-d event1\s*&" $fromdir/runs/* ; then
+ fi 
+ if grep -q -r -e  "sudo python3.*OPENWBBASEDIR/runs/rfid/readrfid.py.*-d event1\s*&" $dir/runs/* ; then
    echo "********************** need patch *********************"
-   grep -r -n -e  "sudo python3.*$OPENWBBASEDIR/runs/rfid/readrfid.py.*-d event1\s*&" $fromdir/runs/* 
+   grep -r -n -e  "sudo python3.*$OPENWBBASEDIR/runs/rfid/readrfid.py.*-d event1\s*&" $dir/runs/* 
    echo " add \" &lt;/dev/null >/dev/null 2>&1 \" before the &"
    Error "Abbruch. Bitte erst korrigieren. atreboot.sh bleibt sonst hängen"
-fi 
+ fi 
+}
  
-meld "----------------------" 
-meld "Stoppe laufende openWB." 
-meld "----------------------" 
-cd $todir
 
-lademodus=$(<$todir/ramdisk/lademodus)
-if (( lademodus != 3 )) ; then
+function StopAktualCharging()
+{
+  local lademodus=$(<$todir/ramdisk/lademodus)
+  deb "Aktueller ladenmodus:$lademodus"
+  if (( lademodus != 3 )) ; then
   meld "Stoppe aktive Ladung."
   mosquitto_pub -t openWB/set/ChargeMode -r -m "3"
   sleep 15
-fi  
-lademodus=$(<$todir/ramdisk/lademodus)
-if (( lademodus != 3 )) ; then
+  fi  
+  lademodus=$(<$todir/ramdisk/lademodus)
+  if (( lademodus != 3 )) ; then
   meld "Stoppe aktive Ladung. (2 Versuch)"
   mosquitto_pub -t openWB/set/ChargeMode -r -m "3"
   sleep 15
-fi  
+ fi  
+ lademodus=$(<$todir/ramdisk/lademodus)
+ if (( lademodus != 3 )) ; then
+    Error "Die aktuelle Ladung läst sich nicht stoppen. Script Abbruch"
+ fi
+}
 
-meld "Stoppe die Regelschleife.."
-echo 1 > $todir/ramdisk/updateinprogress
-echo 1 > $todir/ramdisk/bootinprogress
-mosquitto_pub -t openWB/system/updateInProgress -r -m "1"
-echo "Update im Gange, bitte warten bis die Meldung nicht mehr sichtbar ist" > $todir/ramdisk/lastregelungaktiv
-mosquitto_pub -t "openWB/global/strLastmanagementActive" -r -m "Update im Gange, bitte warten bis die Meldung nicht mehr sichtbar ist"
+function StopRegelSh()
+{
+ meld "Stoppe die Regelschleife.."
+ echo 1 > $todir/ramdisk/updateinprogress
+ echo 1 > $todir/ramdisk/bootinprogress
+ mosquitto_pub -t openWB/system/updateInProgress -r -m "1"
+ echo "Update im Gange, bitte warten bis die Meldung nicht mehr sichtbar ist" > $todir/ramdisk/lastregelungaktiv
+ mosquitto_pub -t "openWB/global/strLastmanagementActive" -r -m "Update im Gange, bitte warten bis die Meldung nicht mehr sichtbar ist"
+ meld "Regelschleife gestoppt"
+ sleep 1
+}
 
-meld "Rename regel.sh damit keine weiteren mehr gestart werden."
-[ -f $todir/regel.sh ] && mv -f $todir/regel.sh $todir/regel.no 
-meld "Regelschleife gestoppt"
-sleep 1
+function PreventRestartRegelNo() # in todir 
+{
+  meld "Rename regel.sh damit keine weiteren mehr gestart werden."
+  [ -f $todir/regel.sh ] && mv -f $todir/regel.sh $todir/regel.no
+}
 
-meld "Stoppe eventuell laufende cron5 und cronnighly jobs.."
-sudo pkill 'cronnighly.sh' >/dev/null
-if [ -f "$todir/ramdisk/cronnighlyruns" ] ; then
+function ReEnableRegelSH()
+{
+ if [ ! -f $todir/regel.sh ] ; then
+   meld "Restore regels.sh from regel.no"
+   mv -f $todir/regel.no $todir/regel.sh
+   sleep 1
+ fi
+}
+
+ 
+function KillCronX() 
+{  
+ meld "Stoppe eventuell laufende cron5 und cronnighly jobs.."
+ sudo pkill 'cronnighly.sh' >/dev/null
+ if [ -f "$todir/ramdisk/cronnighlyruns" ] ; then
   sudo rm -f "$todir/ramdisk/cronnighlyruns"
   meld "cronnighly killed."
-fi
-sudo pkill 'cron5mins.sh' >/dev/null
-if [ -f "$todir/ramdisk/cron5runs" ]  ; then
+ fi
+ sudo pkill 'cron5mins.sh' >/dev/null
+ if [ -f "$todir/ramdisk/cron5runs" ]  ; then
   sudo rm -f "$todir/ramdisk/cron5runs"
   meld "cron5mins killed."
-fi
-sleep 1
+ fi
+ sleep 1
+}
 
+function StopMosquitto()
+{
+ meld "Stoppe MQTT Server Mosquitto "
+ deb "service mosquitto stop" 
+ service mosquitto stop 
+ sleep 1
+}
 
-meld "Stoppe MQTT Server Mosquitto "
-deb "service mosquitto stop" 
-service mosquitto stop 
-sleep 1
+function StartMosquitto()
+{
+ if [ -f $todir/openWB/sav/mosquitto.db ] ; then
+    meld "Restore gespeicherte mosquitto.edb"
+    deb "cp  $todir/openWB/sav/mosquitto.db /var/lib/mosquitto/mosquitto.db"
+    deb "cp  -v -p $todir/openWB/sav/mosquitto.db /var/lib/mosquitto/mosquitto.db"
+ fi
+ meld "Starte Mosquitto neu"
+ service mosquitto start 
+ sleep 1
+}
 
-meld "Stoppe Display (falls eines angeschlossen)."
-deb "service lightdm stop" 
-service lightdm stop
-deb "stoppe chromium" 
-sudo pkill  -f '/usr/lib/chromium-browser/' > /dev/null
-sleep 1
+function StopDisplaywithChrome()
+{
+ meld "Stoppe Display (falls eines angeschlossen)."
+ deb "service lightdm stop" 
+ service lightdm stop
+ deb "stoppe chromium" 
+ sudo pkill  -f '/usr/lib/chromium-browser/' > /dev/null
+ sleep 1
+}
 
-meld "alle anderen openWB-Dienste nun auch anhalten.." 
+function StartDisplaywithChrome()
+{
+ meld "Starte Display neu (macht atreboot.sh nicht)"
+ service lightdm start 
+ sleep 1
+}
 
-cd $todir
-meld "---aktuell aktive Dienste ---"
-sudo ps -efl | grep -E "openWB|runs|tsp|mosquitto" | grep -v grep | grep -v sudo | grep -v restoreschnap 
-meld "---aktuell aktive Dienste ---"
-sleep 1
+function PSallopenWBs()
+{
+ mark=$*
+ meld "$mark"
+ sudo ps -efl | grep -E "openWB|runs|tsp|mosquitto|puller" | grep -v grep | grep -v sudo | grep -v restoreschnap 
+ meld "$mark"
+ sleep 1
+}
 
-
-meld "Stoppe Hintergrundschripte." 
-if [ -f  runs/service.sh ] ; then
+function KillAllOpenWBJobs()
+{
+ cd $todir
+ meld "Stoppe Hintergrundschripte." 
+ if [ -f  runs/services.sh ] ; then
    meld "Stoppe. LSR, modbus, mqtt_sub, isss, rse, rfid, Smarthome, tsp, sysdaem mit runs/services.sh"
-   meld "sudo -u pi runs/services.sh stop all"
-else
+    sudo -u pi runs/services.sh stop all
+ else
   meld "Stoppe. LSR, modbus, mqtt_sub, isss, buchse, rse, rfid, Smarthome jeweils einzeln"
   sudo pkill -f '^python.*/modbusserver.py' > /dev/null
   sudo pkill -f '^python.*/smarthomehandler.py' >/dev/null
@@ -146,54 +190,159 @@ else
   sudo pkill -f 'runs/rse/rseHelper.sh' > /dev/null
   sudo pkill -f '^python.*/rseDaemon.py' > /dev/null
   sudo pkill -f '^python.*/rse.py' > /dev/null
-fi    
+  sudo pkill -f ' runs/sysdaem.sh' > /dev/null
+  sudo -u pi tsp -K
+ fi    
+ sudo pkill -f '^python.*/puller.py' > /dev/null
+ sleep 1
+}
 
-sudo pkill -f '^python.*/puller.py' > /dev/null
 
+
+function RestoreSchnappshut() 
+{
+ local restore_ramdisk=${1:-1}
+ 
+ meld "lösche aktive openWB  (bis auf ramdisk)"
+ rm -r $todir/* 2>/dev/null
+ 
+  # Mache die Ramdisk leer wenn sie mit zurückkopiert wird
+ if (( restore_ramdisk == 1 )) && [ -d $todir/ramdisk ] ; then
+    meld "leere die existierende Ramfdisk"
+    rm -r $todir/ramdisk/* 2>/dev/null
+    deb "Lege sofort wieder die Sperrdateien an."
+    echo 1 > $todir/ramdisk/updateinprogress
+    echo 1 > $todir/ramdisk/bootinprogress
+ fi
+
+ deb "rename regel.sh if needed"
+ [ -f $fromdir/regel.sh ] && mv -f $fromdir/regel.sh $fromdir/regel.no
+
+ (
+ echo "*.bak"
+ echo "regel.no"
+ echo "regel.sh"
+ echo "*.gz"
+ echo "ramdisk/*"
+ ) >$SCRIPT_DIR/excludes
+
+ meld "Exclude-----"
+ more $SCRIPT_DIR/excludes
+ meld "Exclude-----"
+
+ meld "Restore schnappschussdaten mit tar"
+ (
+  cd $fromdir
+  tar -X $SCRIPT_DIR/excludes \
+  --checkpoint=512 \
+  --checkpoint-action=echo="%s %T" \
+  -cf - . | tar -xf - -C $todir/.
+ )
+ meld "Restore regel.sh"
+ cp -p $fromdir/regel.* $todir/.
+
+ meld "lösche Logdatei openWB:log"
+ ln -s -f /var/log/openWB.log $todir/ramdisk
+ [ -f $todir/sav/openWB.log ] && cp -p $todir/sav/openWB.log /var/log/openWB.log
+ ls -l /var/log/openWB.log
+}
+
+
+function pause()
+{
+ read -s -n 1 -p "Press any key to continue . . ."
+ echo ""
+}
+
+
+function restrorefrom
+{
+
+
+deb "$SELF starting ($debug)"
+
+[ ! -d $fromdir ]  &&  Error "Verzeichnis $fromdir existiert nicht."
+
+meld "Schnappschuss $fromdir wiederherstellen." 
+meld "Quelle: $fromdir"
+meld "Ziel  : $todir"
+meld "----------------------" 
 sleep 1
-meld "---noch immer aktive Dienste (sollte nun leer sein bis auf regel.sh) ---"
-sudo ps -efl | grep -E "openWB|runs|tsp|mosquitto" | grep -v grep | grep -v sudo | grep -v restoreschnap
-meld "---noch immer aktive Dienste ---"
-sleep 1
+
+
+cd $base
+
+checkIoRedirection $fromdir
+checkIoRedirection $todir
+ 
+meld "----------------------" 
+meld "Stoppe laufende openWB." 
+meld "----------------------" 
+cd $todir
+
+# Stop Charging in running version in $todir
+StopAktualCharging
+
+StopRegelSh
+
+PreventRestartRegelNo # rename regel.sh to regel.no in todir
+
+KillCronX
+
+StopMosquitto
+
+StopDisplaywithChrome
+
+meld "alle anderen openWB-Dienste nun auch anhalten.." 
+
+cd $todir
+
+PSallopenWBs "---aktuell aktive Dienste ---"
+
+KillAllOpenWBJobs
+
+PSallopenWBs "---noch immer aktive Dienste (sollte nun leer sein bis auf regel.sh) ---"
 
 meld "-------------------"
 meld "openWB ist gestopt"
 meld "-------------------"
-meld "Copy $fromdir zu $todir"
+
+
+pause "Stophere"
 
 # sicherstellen das die regelschleife nicht sofort wieder losläuft
 # wenn regle.sh  zum ziel kopiert wurde in cron gerade zuschlägt 
 [ -f $fromdir/regel.sh ] && mv -f $fromdir/regel.sh $fromdir/regel.no 
 
+meld "Copy $fromdir zu $todir"
 deb "cp -rp openWB_$date openWB"
-# cp -rp openWB_$date openWB
+
+RestoreSchnappshut $restore_ramdisk $restore_data
 
 meld "Copy done ---------"
 meld "-------------------"
 
 
-meld "Starte Display neu (macht atreboot.sh nicht)"
-service lightdm start 
-sleep 1
-meld "Starte Mosquitto neu"
-service mosquitto start 
-sleep 1
+StartDisplaywithChrome
 
-if [ ! -f $todir/regel.sh ] ; then
-   meld "Restore regels.sh from regel.no"
-   mv -f $todir/regel.no $todir/regel.sh
-fi
+StartMosquitto
+
+ReEnableRegelSH
  
 
-meld "Trigger neustart in openWB um die Dienste wieder zu starten."
+meld "Trigger Neustart der openWB mit atreboot.sh."
 meld "-------------------"
 cd $todir
 sudo -u pi /bin/bash -c "runs/atreboot.sh 2>&1 " | tee  -a /var/log/openWB.log 
 meld "-------------------"
-meld "---wieder aktive Dienste ---"
-sudo ps -efl | grep -E "openWB|runs|tsp" | grep -v grep | grep -v sudo | grep -v restoreschnap
-meld "---wieder aktive Dienste ---"
-sleep 1
+
+PSallopenWBs "---wieder aktive Dienste ---"
+
 meld "Fertig."
 meld "::END::"
+}
+
+
+
+restrorefrom
 
